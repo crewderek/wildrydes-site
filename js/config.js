a6d96a5e3d532a43fb50b9e7d1faee1567095307
Derek Crew <crewderek@yahoo.com>
2023-02-22 18:16:07 -0700

    Client secret changed

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-west-2_11pkFG6Hl', // e.g. us-west-2_11pkFG6Hl
-        userPoolClientId: '3449j8v2seh2ilma7iiehv08bv', // e.g. 3449j8v2seh2ilma7iiehv08bv
+        userPoolClientId: '2ogug8eccbemcq5tdl9insf04l', // e.g. 3449j8v2seh2ilma7iiehv08bv
         region: 'us-west-2' // e.g. us-west-2
     },
     api: {
