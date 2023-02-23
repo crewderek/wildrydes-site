84424c8c2d5dd0d8f0724df21cdb48837273a23f
Derek Crew <crewderek@yahoo.com>
2023-02-22 17:39:38 -0700

    User pool added

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-2_11pkFG6Hl', // e.g. us-west-2_11pkFG6Hl
+        userPoolClientId: '3449j8v2seh2ilma7iiehv08bv', // e.g. 3449j8v2seh2ilma7iiehv08bv
+        region: 'us-west-2' // e.g. us-west-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
